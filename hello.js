c447e940464f244117ec2da6336b5a16c8d7ef10
Sam Windsor <sam@samwindsor.com>
2022-03-24 13:40:03 -0400

    Intial commit

---
+++ hello.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var url = require('url');
+
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	var q = url.parse(req.url, true).query;
+	var dates = q.year;
+	res.write(dates);
+	res.end();
+}).listen(8080);
